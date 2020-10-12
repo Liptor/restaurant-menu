16fff65f9503df9a87fe9cdb9f854d1b889569e2
Ivan <ivan.ingov@gmail.com>
2020-10-12 17:56:28 +0300

    Updates

---
+++ precache-manifest.25e9b7da746a1e5ac74b0f1256c24fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab0734407074f934ff68db636419e00",
+    "url": "/restaurant-menu/index.html"
+  },
+  {
+    "revision": "2ec8502a0f897c753d92",
+    "url": "/restaurant-menu/static/css/main.a80fe6ff.chunk.css"
+  },
+  {
+    "revision": "0ec14ca0a00e0f42094f",
+    "url": "/restaurant-menu/static/js/2.68eaa2da.chunk.js"
+  },
+  {
+    "revision": "2ec8502a0f897c753d92",
+    "url": "/restaurant-menu/static/js/main.43f1466f.chunk.js"
+  },
+  {
+    "revision": "b20dd83e36889b5892dd",
+    "url": "/restaurant-menu/static/js/runtime~main.07df0635.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/restaurant-menu/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/restaurant-menu/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
